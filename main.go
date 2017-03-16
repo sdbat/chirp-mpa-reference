1ccf282ca7fa85c3dba7db2329371ed14c0d0020
rjp <rjprzy@gmail.com>
2017-03-16 16:42:23 -0700

    major additions: use Chirp TOFs, ignore IQ data, adaptive glitch rejection

--- main.go
+++ main.go
@@ -36,6 +36,14 @@ type room_anemometer struct {
 	s_matrix [4][4]float32
 	//tofs in microseconds
 	tof_matrix [4][4]float32
+	//expected tofs
+	tof_expected [4][4]float32
+	//tof_expected IIR coefficient
+	tof_e_coeff [4][4]float32
+	//allowed range of tofs (can be +/- of this value)
+	tof_range [4][4]float32
+	//number of good tofs
+	count_good [4][4]int32
 	//component velocities in m/s
 	vel_matrix [4][4]float32
 	//scale factors from components to cardinal
@@ -64,6 +72,7 @@ func NewDuctAnemometer() *room_anemometer {
 	ra := room_anemometer{}
 	ra.num_samples = 0
 	ra.mode = 2
+
 	//initialize port to index according to geometry
 	ra.port_to_idx[0] = 2 //port 0 downstream top
 	ra.port_to_idx[1] = 1 //port 1 is upstream bottom
@@ -81,6 +90,7 @@ func NewDuctAnemometer() *room_anemometer {
 	for i := 0; i < 4; i++ {
 		ra.s_matrix[i][i] = 0.0
 		ra.tof_matrix[i][i] = 1.0e-12
+
 	}
 
 	for i := 0; i < 4; i++ {
@@ -88,7 +98,12 @@ func NewDuctAnemometer() *room_anemometer {
 			ra.s_matrix[j][i] = ra.s_matrix[i][j]
 		}
 		for k := 0; k < 4; k++ {
+			ra.tof_e_coeff[i][k] = 0.5
+			ra.count_good[i][k] = 1
+			ra.tof_range[i][k] = float32(5.0 + 10.0/ra.count_good[i][k])
 			ra.tof_matrix[i][k] = ra.s_matrix[i][k] / 343.0
+			ra.tof_expected[i][k] = ra.tof_matrix[i][k]
+			ra.tof_expected[i][k] = ra.tof_matrix[i][k]
 		}
 	}
 
@@ -117,6 +132,7 @@ func NewRoomAnemometer() *room_anemometer {
 	ra := room_anemometer{}
 	ra.num_samples = 0
 	ra.mode = 1
+
 	//initialize port to index according to geometry
 	ra.port_to_idx[0] = 1 //port 0 is B in doc
 	ra.port_to_idx[1] = 3 //port 1 is D
@@ -127,8 +143,12 @@ func NewRoomAnemometer() *room_anemometer {
 	//Other matrixes are already initialized to 0
 	for i := 0; i < 4; i++ {
 		for j := 0; j < 4; j++ {
+			ra.tof_e_coeff[i][j] = 0.5
 			ra.s_matrix[i][j] = 60000.0
-			ra.tof_matrix[i][j] = 174.92
+			ra.count_good[i][j] = 1
+			ra.tof_matrix[i][j] = ra.s_matrix[i][j] / 343.0
+			ra.tof_expected[i][j] = ra.tof_matrix[i][j]
+			ra.tof_range[i][j] = float32(5.0 + 10.0/ra.count_good[i][j])
 			if i == j {
 				ra.s_matrix[i][j] = 0.0
 				ra.tof_matrix[i][j] = 1.0e-12 //prevent divide by zero
@@ -161,6 +181,26 @@ func NewRoomAnemometer() *room_anemometer {
 		}
 	}
 	return &ra
+}
+
+func (ra *room_anemometer) setToFs(tof_us float32, txi int32, rxi int32, toprint bool) {
+	if tof_us < ra.tof_expected[txi][rxi]+ra.tof_range[txi][rxi] && tof_us > ra.tof_expected[txi][rxi]-ra.tof_range[txi][rxi] {
+		//ra.tof_matrix[txi][rxi] = tof_us
+		ra.tof_e_coeff[txi][rxi] = 0.99 - 0.5/float32(ra.count_good[txi][rxi])
+		ra.tof_expected[txi][rxi] = ra.tof_expected[txi][rxi]*ra.tof_e_coeff[txi][rxi] + tof_us*(1.0-ra.tof_e_coeff[txi][rxi])
+		ra.tof_matrix[txi][rxi] = tof_us
+		ra.tof_range[txi][rxi] = 5.0 + 10.0/float32(ra.count_good[txi][rxi])
+		ra.count_good[txi][rxi]++
+		if toprint {
+			fmt.Printf("TOF expected[%d][%d]: %0.3f, range: %0.3f, coeff: %0.3f, good: %d\n", txi, rxi, ra.tof_expected[txi][rxi], ra.tof_range[txi][rxi], ra.tof_e_coeff[txi][rxi], ra.count_good[txi][rxi])
+		}
+
+	} else {
+		if toprint {
+			fmt.Printf("Bad sample, TOF expected[%d][%d]: %0.3f, range: %0.3f, coeff: %0.3f, good: %d\n", txi, rxi, ra.tof_expected[txi][rxi], ra.tof_range[txi][rxi], ra.tof_e_coeff[txi][rxi], ra.count_good[txi][rxi])
+		}
+	}
+
 }
 
 func (ra *room_anemometer) cardinalVelocities() {
@@ -220,8 +260,8 @@ func Initialize(emit l7g.Emitter) {
 func OnNewData(popHdr *l7g.L7GHeader, h *l7g.ChirpHeader, emit l7g.Emitter) {
 	// Define some magic constants for the algorithm
 	duct := false
-	magic_count_tx := -3.125
-	threshold := uint64(1400 * 1400)
+	magic_count_tx := -4.8 //-3.125
+	//	threshold := uint64(1400 * 1400)
 	//	fmt.Printf(".\n")
 	//Room anemometers have build numbers like 110, 120, 130
 	//duct anemometers have build numbers like 115, 125, 135
@@ -293,46 +333,48 @@ func OnNewData(popHdr *l7g.L7GHeader, h *l7g.ChirpHeader, emit l7g.Emitter) {
 		//Find the largest complex magnitude (as a square). We do this like this
 		//because it more closely mirror how it would be done on an embedded device
 		// (actually because I copied the known-good firestorm implementation)
-		magsqr := make([]uint64, 16)
-		magmax := uint64(0)
-		for i := 0; i < 16; i++ {
-			magsqr[i] = uint64(int64(qz[i])*int64(qz[i]) + int64(iz[i])*int64(iz[i]))
-			if magsqr[i] > threshold {
-				if magsqr[i] < magmax {
-					break
-				} else {
-					magmax = magsqr[i]
-				}
-			}
-		}
+		//		magsqr := make([]uint64, 16)
+		//		magmax := uint64(0)
+		//		for i := 0; i < 16; i++ {
+		//			magsqr[i] = uint64(int64(qz[i])*int64(qz[i]) + int64(iz[i])*int64(iz[i]))
+		//			if magsqr[i] > threshold {
+		//				if magsqr[i] < magmax {
+		//					break
+		//				} else {
+		//					magmax = magsqr[i]
+		//				}
+		//			}
+		//		}
 		txi := ra.port_to_idx[h.Primary]
 		rxi := ra.port_to_idx[set]
 		//		fmt.Printf("Tx: %d Rx: %d Tdx: %d Rdx: %d\n", h.Primary, set, txi, rxi)
 		if isprimary == false {
 			//Find the first index to be greater than half the max (quarter the square)
-			quarter := magmax / 4
-			less_idx := 0
-			greater_idx := 0
-			for i := 0; i < 16; i++ {
-				if magsqr[i] < quarter {
-					less_idx = i
-				}
-				if magsqr[i] > quarter {
-					greater_idx = i
-					break
-				}
-			}
+			//			quarter := magmax / 4
+			//			less_idx := 0
+			//			greater_idx := 0
+			//			for i := 0; i < 16; i++ {
+			//				if magsqr[i] < quarter {
+			//					less_idx = i
+			//				}
+			//				if magsqr[i] > quarter {
+			//					greater_idx = i
+			//					break
+			//				}
+			//			}
 
-			//Convert the squares into normal floating point
-			less_val := math.Sqrt(float64(magsqr[less_idx]))
-			greater_val := math.Sqrt(float64(magsqr[greater_idx]))
-			half_val := math.Sqrt(float64(quarter))
+			//			//Convert the squares into normal floating point
+			//			less_val := math.Sqrt(float64(magsqr[less_idx]))
+			//			greater_val := math.Sqrt(float64(magsqr[greater_idx]))
+			//			half_val := math.Sqrt(float64(quarter))
+			//			lerp_idx := float64(less_idx) + (half_val-less_val)/(greater_val-less_val)
 			//CalPulse is in microseconds
 			freq := float64(tof_sf) / 2048 * float64(h.CalRes[set]) / (float64(h.CalPulse) / 1000)
 			//Linearly interpolate the index (the index is related to time of flight because it is regularly sampled)
-			lerp_idx := float64(less_idx) + (half_val-less_val)/(greater_val-less_val)
+
 			//Fudge the result with magic_count_tx and turn into time of flight
-			tof := (lerp_idx + float64(magic_count_tx)) / freq * 8
+			//			tof := (lerp_idx + float64(magic_count_tx)) / freq * 8
+			tof_chip := (float64(tof_est) + magic_count_tx*256.0) / (freq * 32.0) * 1000000.0
 			_ = tof_est
 			_ = intensity
 			//		fmt.Printf("SEQ %d ASIC %d primary=%d\n", h.Seqno, set, h.Primary)
@@ -342,21 +384,22 @@ func OnNewData(popHdr *l7g.L7GHeader, h *l7g.ChirpHeader, emit l7g.Emitter) {
 
 				//We print these just for fun / debugging, but this is not actually emitting the data
 				fmt.Printf("SEQ %d ASIC %d primary=%d\n", h.Seqno, set, h.Primary)
-				fmt.Println("lerp_idx: ", lerp_idx)
+				//				fmt.Println("lerp_idx: ", lerp_idx)
 				fmt.Println("tof_sf: ", tof_sf)
 				fmt.Println("freq: ", freq)
-				fmt.Printf("tof: %.2f us\n", tof*1000000)
+				//				fmt.Printf("tof: %.2f us\n", tof*1000000)
 				fmt.Println("intensity: ", intensity)
-				fmt.Println("tof chip estimate: ", tof_est)
-				fmt.Println("tof 50us estimate: ", lerp_idx*50)
-				fmt.Println("data: ")
-				for i := 0; i < 16; i++ {
-					fmt.Printf(" [%2d] %6d + %6di (%.2f)\n", i, qz[i], iz[i], math.Sqrt(float64(magsqr[i])))
-				}
-				fmt.Println(".")
+				fmt.Println("tof chip raw: ", tof_est)
+				fmt.Println("tof chip estimate: ", tof_chip)
+				//				fmt.Println("tof 50us estimate: ", lerp_idx*50)
+				//				fmt.Println("data: ")
+				//				for i := 0; i < 16; i++ {
+				//					fmt.Printf(" [%2d] %6d + %6di (%.2f)\n", i, qz[i], iz[i], math.Sqrt(float64(magsqr[i])))
+				//				}
+				//				fmt.Println(".")
 			}
+			ra.setToFs(float32(tof_chip), txi, rxi, toprint)
 
-			ra.tof_matrix[txi][rxi] = float32(tof * 1000000.0)
 			ra.vel_matrix[txi][rxi] = 0.5 * (ra.s_matrix[txi][rxi]/ra.tof_matrix[txi][rxi] -
 				ra.s_matrix[rxi][txi]/ra.tof_matrix[rxi][txi])
 
@@ -368,25 +411,25 @@ func OnNewData(popHdr *l7g.L7GHeader, h *l7g.ChirpHeader, emit l7g.Emitter) {
 			odata.Tofs = append(odata.Tofs, l7g.TOFMeasure{
 				Src: int(h.Primary),
 				Dst: set,
-				Val: tof * 1000000})
+				Val: tof_chip})
 		} else {
 			//isprimary == true
 			sum := float32(0)
-			for i := 0; i < 16; i++ {
-				sum = sum + float32(magsqr[i])
-			}
+			//			for i := 0; i < 16; i++ {
+			//				sum = sum + float32(magsqr[i])
+			//			}
 			ra.trace_sum[set] = sum / float32(16.0*32768.0*32768.0)
 		}
 
 	} //end for each of the four measurements
 	ra.num_samples = ra.num_samples + 1
 	ra.cardinalVelocities()
-
-	ra.filterVelocity(0.99)
+	coeff := float32(0.95)
+	ra.filterVelocity(coeff)
 	ra.filterTrace(0.90, h.Primary)
-	ra.calibrateVelocity(500)
+	ra.calibrateVelocity(int32(5.0 / (1.0 - coeff)))
 
-	if popHdr.Srcmac == "99706f326a21465a" {
+	if popHdr.Srcmac == "98bed0134528465a" {
 		fmt.Printf("%d, %.3f, %.3f, %.3f\n", ra.num_samples, ra.vxyz_cal[0], ra.vxyz_cal[1], ra.vxyz_cal[2])
 	}
 
